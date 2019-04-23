b3d8af0b19cad7ed143972393710e613eabfb53f
JJohnsonChan <asunmoon25@gmail.com>
2019-04-23 18:10:31 +0800

    first commit

---
+++ JJHardwareDecode/JJHardwareDecode.h
@@ -0,0 +1,19 @@
+//
+//  JJHardwareDecode.h
+//  JJHardwareDecode
+//
+//  Created by Johnson Chan on 2019/4/23.
+//  Copyright © 2019 Johnson Chan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JJHardwareDecode.
+FOUNDATION_EXPORT double JJHardwareDecodeVersionNumber;
+
+//! Project version string for JJHardwareDecode.
+FOUNDATION_EXPORT const unsigned char JJHardwareDecodeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JJHardwareDecode/PublicHeader.h>
+
+
